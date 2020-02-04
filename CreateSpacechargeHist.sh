7d09cc6891720f2ce879e858f1dd2faa4f2ee16f
Ross Corliss <rcorliss@mit.edu>
2020-02-04 01:18:49 -0500

    changed the rcas default filename

--- CreateSpacechargeHist.sh
+++ CreateSpacechargeHist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #at home:
-dir="/osx_sphenix/"
-basename="G4Hits_sHijing_9-11fm_"
+#dir="/osx_sphenix/"
+#basename="G4Hits_sHijing_9-11fm_"
 
 #at rcas:
-#dir="/sphenix/sim/sim01/production/sHijing/new/fm_0-12/"
-#basename="G4Hits_sHijing_0-12fm_"
+dir="/sphenix/sim/sim01/sphnxpro/sHijing/2019-07-28/fm_9-11/"
+basename="G4Hits_sHijing_9-11fm_"
 maxend=200
 for ((r=0;r<=maxend;r=r+100)); do
     rp=$(($r+100))

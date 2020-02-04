7b7fc8470aa3974fa64de905b403c2f2ef511119
Ross Corliss <rcorliss@mit.edu>
2020-02-04 11:50:46 -0500

    added proper volume normalization and fixed some units errors

--- CreateSpacechargeHist.C
+++ CreateSpacechargeHist.C
@@ -11,7 +11,7 @@ R__LOAD_LIBRARY(libphg4hit.so)
 //need to include something to read DST.TPC.G4HIT_TPC
 //and maybe 'G4HIT_ABSORBER_TPC' as well?
 
-void CreateSpacechargeHist(const char *dirname, const char *filename, int istart=0){
+void CreateSpacechargeHist(const char *dirname, const char *filename, int istart=0, bool saveTree=false){
   printf("are you running with the sphenix env?  This probably doesn't work without that!\n");
 
   gSystem->Load("libg4testbench.so");
@@ -34,7 +34,7 @@ void CreateSpacechargeHist(const char *dirname, const char *filename, int istart
 
 
   float us=1.0,ms=us*1e3,s=ms*1e3;
-  float um=1.0, mm=um*1e3, cm=mm*10,m=mm*1e3;
+  float um=1e-4, mm=um*1e3, cm=mm*10,m=mm*1e3; //changed to make 'cm' 1.0, for convenience.
   float Hz=1/s, kHz=1/ms, MHz=1/us;
   //used two ways:  1) to apply units to variables when defined
   //                2) to divide by certain units so that those variables are expressed in those units.
@@ -44,9 +44,9 @@ void CreateSpacechargeHist(const char *dirname, const char *filename, int istart
   float ampIBFfrac=0.01;
   float ionsPerEle=ampGain*ampIBFfrac;
   float mbRate=22*kHz;
-  float z_rdo=100*cm;
+  float z_rdo=105.5*cm;
   float rmin=20*cm;
-  float rmax=80*cm;
+  float rmax=78*cm;
 
   double Ne_dEdx = 1.56;   // keV/cm
   double CF4_dEdx = 7.00;  // keV/cm
@@ -59,50 +59,91 @@ void CreateSpacechargeHist(const char *dirname, const char *filename, int istart
 
   
   TFile *outfile=TFile::Open(Form("%s.rcc_sc.hist.root",filename),"RECREATE");
-  TH3D *hCharge=new TH3D("sphenix_minbias_charge","blah",360,0,6.28319,100,rmin,rmax,100,0,z_rdo);
-  
+  int nr=159;
+  int nphi=360;
+  int nz=110;
+  TH3D *hCharge=new TH3D("sphenix_minbias_charge","SC (ions) per cm^3;phi (rad);r (cm);z (cm)",nphi,0,6.28319,nr,rmin/cm,rmax/cm,110,0,z_rdo/cm);
+  double hrstep=(rmax-rmin)/cm/nr;
+  double hphistep=6.28319/nphi;
+  double hzstep=z_rdo/cm/nz;
+  TTree *rawHits;
+
 
   //to fully populate the detector half, we need events covering t0=0 to t0=z_rdo/vIon.  These occur at mbRate, so:
   printf("vIon=%f cm/s\tz=%f cm\t rate=%f kHz\n ==> need z/v*r=%f events to cover the detector. T has %d entries.\n",vIon/(cm/s),z_rdo/(cm),mbRate/(kHz),(z_rdo/vIon*mbRate), neve);
   
-  float x,y,z;
+  float x,y,z,zibf,zprim;
   float r,phi;
   float ne;
 
   float driftedZ;
   int testi=5;
+  int i;
+  if (saveTree){
+    rawHits=new TTree("hTree","tpc hit tree for ionization");
+    rawHits->Branch("x",&x);
+    rawHits->Branch("y",&y);
+    rawHits->Branch("zorig",&z);
+    rawHits->Branch("zibf",&zibf);
+    rawHits->Branch("zprim",&zprim);
+    rawHits->Branch("r",&r);
+    rawHits->Branch("phi",&phi);
+    rawHits->Branch("ev",&i);
+    rawHits->Branch("ne",&ne);
+  }
+
+  
   T->GetEntry(testi);
   printf("after GetEntry(%d), eleHits pointer is %p\n",testi,(void *)eleHits);
 
   //return;
-  for (int i=1;i<neve;i++){
+
+  for (i=1;i<neve;i++){
     T->GetEntry(i);
     //eventually I ought to roll a random number for each bunch crossing and use that to detemrine the number of events, but for now I just take one per minbiasRate.
     //must be sure to convert the incoming positions into local units.
     //load prim*
     //printf("loading eve=%d\n",i);
     float t0=(i+istart)/mbRate;
-    driftedZ=t0*vIon /(cm);//drift position in cm
-
+    driftedZ=t0*vIon;//drift position in local units
     PHG4HitContainer::ConstRange range=eleHits->getHits();
     assert(range);
+
     float f=0.5;;//for now, just pick the middle of the hit.  Do better later.
+
+    
     for (PHG4HitContainer::ConstIterator hiter=range.first;hiter!=range.second;hiter++)
       {
 	ne=hiter->second->get_eion()*Tpc_ElectronsPerGeV;
-	x = hiter->second->get_x(0) + f * (hiter->second->get_x(1) - hiter->second->get_x(0));
-	y = hiter->second->get_y(0) + f * (hiter->second->get_y(1) - hiter->second->get_y(0));
-	z = hiter->second->get_z(0) + f * (hiter->second->get_z(1) - hiter->second->get_z(0));
+	//load the three coordinates in with units of cm.
+	x = (hiter->second->get_x(0) + f * (hiter->second->get_x(1) - hiter->second->get_x(0)))*(cm);
+	y = (hiter->second->get_y(0) + f * (hiter->second->get_y(1) - hiter->second->get_y(0)))*(cm);
+	z = (hiter->second->get_z(0) + f * (hiter->second->get_z(1) - hiter->second->get_z(0)))*(cm);
+	if (z<0) continue;
 	r=sqrt(x*x+y*y);
 	phi=atan2(x,y);
-	hCharge->Fill(phi,r,z-driftedZ,1); //primary ion, drifted by t0
-	hCharge->Fill(phi,r,z_rdo-driftedZ,ionsPerEle); //amp ion, drifted by t0
+	zprim=z-driftedZ;
+	zibf=z_rdo-driftedZ;
+	if (phi<0) phi+=6.28319;
+	//compute the bin volume:
+	int bin=hCharge->FindBin(phi,r/(cm),zprim/(cm));
+	double hr=hCharge->GetYaxis()->GetBinLowEdge(bin);
+	double vol=(hzstep*hphistep*(2*hr+hrstep)*hrstep)/cm/cm/cm;
+
+	hCharge->Fill(phi,r/(cm),zprim/(cm),ne/vol); //primary ion, drifted by t0, in cm
+	hCharge->Fill(phi,r/(cm),zibf/(cm),ne*ionsPerEle/vol); //amp ion, drifted by t0, in cm
+	if (saveTree){
+	  rawHits->Fill();
+	}
     }
 
     
   }
   outfile->cd();
   hCharge->Write();
+  if (saveTree){
+    rawHits->Write();
+  }
   outfile->Close();
   return;
 }

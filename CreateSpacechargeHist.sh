ca6967e36912eda5350325512c5bf99b3d1856ae
Ross Corliss <rcorliss@mit.edu>
2020-02-04 01:39:14 -0500

    extended the range of spacecharge hist

--- CreateSpacechargeHist.sh
+++ CreateSpacechargeHist.sh
@@ -7,9 +7,9 @@
 #at rcas:
 dir="/sphenix/sim/sim01/sphnxpro/sHijing/2019-07-28/fm_9-11/"
 basename="G4Hits_sHijing_9-11fm_"
-maxend=200
+maxend=3000
 for ((r=0;r<=maxend;r=r+100)); do
     rp=$(($r+100))
     fname=$basename`printf "%05d\n" $r`_`printf "%05d\n" $rp`.root
-    echo root -b -q CreateSpacechargeHist.C\(\"$dir\",\"$fname\",$r\)
+    root -b -q CreateSpacechargeHist.C\(\"$dir\",\"$fname\",$r\)
 done

603e1061a4878057f698eea34d91d7609adc5d38
Ross Corliss <rcorliss@mit.edu>
2020-02-03 23:51:15 -0500

    added path for rcas use

--- CreateSpacechargeHist.sh
+++ CreateSpacechargeHist.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-dir="/osx_sphenix/"
-basename="G4Hits_sHijing_9-11fm_"
-maxend=20
+#at home: dir="/osx_sphenix/"
+#basename="G4Hits_sHijing_9-11fm_"
+
+#at rcas:
+dir="/sphenix/sim/sim01/production/sHijing/new/fm_0-12/"
+basename="G4Hits_sHijing_0-12fm_"
+maxend=200
 for ((r=0;r<=maxend;r=r+100)); do
     rp=$(($r+100))
     fname=$basename`printf "%05d\n" $r`_`printf "%05d\n" $rp`.root

cfe49f71710d88cece60b044512a91137e3d1ee1
Ross Corliss <rcorliss@mit.edu>
2021-11-29 12:51:38 -0500

    added loop over files to the histogram renaming.

--- renameHistograms.C
+++ renameHistograms.C
@@ -2,9 +2,11 @@
 This is a little toy to rename histograms and only save a subset from one file to another
  */
 
-std::string infileName="";
-std::string outfileName="";
+//std::string infileName="";
+//std::string outfileName="";
 
+
+vector<std::pair<std::string,std::string>> fileName;
 vector<std::pair<std::string,std::string>> histName;
 
 int main(){
@@ -20,21 +22,39 @@ void renameHistograms(){
   histName.push_back(std::make_pair("hIntDistortionPosP","hIntDistortionP_posz"));
   histName.push_back(std::make_pair("hIntDistortionPosZ","hIntDistortionZ_posz"));
 
+  fileName.push_back(std::make_pair("/star/u/rcorliss/sphenix/workfest2021/full_distortion.workfest2021.distortion_map.hist.root",
+				    "/star/u/rcorliss/sphenix/trackingStudySampleNov2021/actual_event.distortion_map.hist.root"));
+  fileName.push_back(std::make_pair("/star/u/rcorliss/sphenix/workfest2021/empty_distortion.workfest2021.distortion_map.hist.root",
+				    "/star/u/rcorliss/sphenix/trackingStudySampleNov2021/static_only.distortion_map.hist.root"));
+  fileName.push_back(std::make_pair("/star/u/rcorliss/sphenix/workfest2021/average_distortion.workfest2021.distortion_map.hist.root",
+				    "/star/u/rcorliss/sphenix/trackingStudySampleNov2021/average_event.distortion_map.hist.root"));
+  /*
+    fileName.push_back(std::make_pair("/star/u/rcorliss/sphenix/workfest2021/",
+				    "/star/u/rcorliss/sphenix/workfest2021/"));
+  */
 
-  TFile infile=TFile::Open(infileName,"READ");
-  TFile outfile=TFile::Open(outfileName,"RECREATE");
+  
+  TFile *infile;=TFile::Open(infileName,"READ");
+  TFile *outfile;=TFile::Open(outfileName,"RECREATE");
 
   TH1* hin,hout;
 
-  for (int i=0;i<histName.length();i++){
-    hin=(TH1*)infile->Get(histName[i].first());
-    hout=(TH1*)hin->Clone(histName[i].second());
-  }
+  for (int f=0;f<fileName.length(),f++){
 
-  outfile->cd();  
-  for (int i=0;i<histName.length();i++){
-    hout->Write();
+    infile=TFile::Open(fileName[f].first(),"READ");
+    outfile=TFile::Open(fileName[f].second(),"RECREATE");
+    
+    for (int i=0;i<histName.length();i++){
+      hin=(TH1*)infile->Get(histName[i].first());
+      hout=(TH1*)hin->Clone(histName[i].second());
+    }
+    
+    outfile->cd();  
+    for (int i=0;i<histName.length();i++){
+      hout->Write();
+    }
+    outfile->Close();
+    infile->Close();
   }
-  outfile->Close();
-  return;
+    return;
 }

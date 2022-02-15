7f0980cd88a5615200b2a0cc8f15eda817de267c
Ross Corliss <rcorliss@mit.edu>
2022-02-15 10:43:31 -0500

    added invert bash script

---
+++ invertHistograms.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#usage:  ./invertHistograms.sh [searchstring] [outputdir]
+
+#check arguments:
+if [ $# -lt 2 ]
+   then echo "usage: ./invertHistograms.sh [searchstring] [outputdir]"
+fi
+
+#echo $# minus 1 is `expr $# - 1`
+nFiles=`expr $# - 1`
+echo found $nFiles files.
+outputdir=${!#}
+echo outputdir is $outputdir
+
+
+for i in $(seq 1 $nFiles)
+do
+    #echo processing file $i
+    inputname=${!i}
+    basefilename=`basename $inputname`
+    outputname=`echo "${basefilename/.distortion_map.hist.root/.invert.distortion_map.hist.root}"`
+    outputname=${outputdir}/${outputname}
+    #echo $inputname becomes $outputname
+    
+done
+
+exit

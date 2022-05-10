fbc6ea4ae0fd5151e4d50ae576192555d0f07fe4
Ross Corliss <rcorliss@mit.edu>
2022-05-10 10:50:45 -0400

    debug

--- MakeFlatDummyCorrection.C
+++ MakeFlatDummyCorrection.C
@@ -2,7 +2,7 @@
 //needs bounds such that all valid positions in the tpc are in the center of the 3x3x3 bins so that interpolation works everwhere.
 
 void MakeFlatDummyCorrection(const char* filebase="dummyCorrection", const float rval=0,const float phival=0, const float zval=0){
-  printf("Writing correction file %s with correction =(%2E,%2E,%2E)(r,phi,z)\n",filename,rval,phival,zval);
+  printf("Writing correction file %s with correction =(%.2E,%.2E,%.2E)(r,phi,z)\n",filebase,rval,phival,zval);
   enum coord{R=0,P=1,Z=2};
   TH3F *hFlat[2][3];
   float val[3];
@@ -32,23 +32,24 @@ void MakeFlatDummyCorrection(const char* filebase="dummyCorrection", const float
   const std::array<const std::string,3> axis = {{ "R", "P", "Z" }};
   for (int i=0;i<2;i++){
     for (int j=0;j<3;j++){
-      hFlat[i][j]=new TH3F(Form("hIntDistortion%s%s",axis,extension),
+      hFlat[i][j]=new TH3F(Form("hIntDistortion%s%s",axis[j].c_str(),extension[i].c_str()),
 			   Form("Dummy Correction for %s (%s), %2E everywhere",axis[j].c_str(),extension[i].c_str(),val[i]),
 			   /*phi in rads.*/3,low[P],high[P],
 			   /*r in cm.*/3,low[R],high[R],
 			   /*z in cm*/3,low[Z],high[Z]);
 	//now fill the bins:
-	for (int k=0;k<hFlat[i][j]->GetNcells(),k++){
+      for (int k=0;k<hFlat[i][j]->GetNcells();k++){
 	  hFlat[i][j]->SetBinContent(k,val[j]);
 	}	  
     }
   }
 
   //now write these to the desired file:
-  TFile *output=TFile::Open(Form("%s.hist.root",filename),"RECREATE");
+  TFile *output=TFile::Open(Form("%s.hist.root",filebase),"RECREATE");
   for (int i=0;i<2;i++){
     for (int j=0;j<3;j++){
       hFlat[i][j]->Write();
     }
   }
+  return;
 }

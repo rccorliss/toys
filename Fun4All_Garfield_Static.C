1ab4b29c297fbeffb5f27a52da61d369212a44b1
Ross Corliss <rcorliss@mit.edu>
2026-08-03 14:33:20 -0400

    newer version?

--- Fun4All_Garfield_Static.C
+++ Fun4All_Garfield_Static.C
@@ -30,7 +30,7 @@
 #include <phool/recoConsts.h>
 #include <GlobalVariables.C>
 
-#include <phgarfield/PHGarfield.h>
+//#include <phgarfield/PHGarfield.h>
 
 #include <TPolyLine3D.h>
 #include <TPolyLine.h>
